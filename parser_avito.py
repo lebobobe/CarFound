7a5f904c9d94f82d1e205b0d450eae2e24a50adc
Alexandr Sobko <alexfreetibet@gmail.com>
2021-03-07 18:23:49 +0700

    методы для получения ссылок на новые объявления

--- parser_avito.py
+++ parser_avito.py
@@ -1,36 +1,53 @@
+from collections import namedtuple
+
+import bs4
 import requests
 
 
 class AvitoParser:
     def __init__(self):
+        self.url = 'https://www.avito.ru'
         self.session = requests.Session()
-        self.session.headers = {
-            'User-Agent': (
-                'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N)'
-                'AppleWebKit/537.36 (KHTML, like Gecko)'
-                'Chrome/88.0.4324.190 Mobile Safari/537.36'
-            ),
-            'Accept-Language': 'ru',
-        }
 
-    def get_page(self, city: str, category: str = 'avtomobili', model: str = None, page: int = None):
+    def _get_page(self, city: str, category: str = 'avtomobili', model: str = None, radius: int = 0, page: int = None):
+        """
+        Возвращает html списка объявлений отсортированных по дате
+        Все параметры необходимо передавать в нижнем регистре
+        """
+        url = f'{self.url}/{city}/{category}'
         params = {
             'cd': 1,            # непонятно, есть если не выбрана марка авто
-            'radius': 200,      # Радиус поиска вокруг города в км
+            'radius': radius,   # Радиус поиска вокруг города в км
             's': 104,           # Сортировка (104 по дате, 1 дешевле, 2 дороже)
         }
+
         if page and page > 1:
             params['p'] = page
 
-        url = f'https://www.avito.ru/{city}/{category}'
         if model:
             url = f'{url}/{model}'
+            params.pop('cd')
 
-        r = self.session.get(url, params=params)
-        return r.text
+        try:
+            r = self.session.get(url, params=params)
+            r.raise_for_status()
+            return r.text
+        except (requests.RequestException, ValueError):
+            return False
+
+    def _get_new_links(self, city: str):
+        text = self._get_page(city=city)
+        soup = bs4.BeautifulSoup(text, 'lxml')
+        titles = soup.find_all('div', class_='iva-item-titleStep-2bjuh')
+        for item in titles:
+            block = item.find('a')
+            item_url = f"https://www.avito.ru{block.get('href')}"
+            print(item_url)
+
+
 
 
 if __name__ == '__main__':
     parser = AvitoParser()
-    print(parser.get_page(city="Tomsk"))
+    parser._get_new_links(city="tomsk")
 
