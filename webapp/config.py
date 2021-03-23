2101a8c8bc1fd4c17cc3ce58c5b7fc1e33663e69
Alexandr Sobko <alexfreetibet@gmail.com>
2021-03-23 22:34:15 +0700

    создание бд

--- webapp/config.py
+++ webapp/config.py
@@ -2,5 +2,6 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
+DEBUG = True
 SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, '..', 'webapp.db')}"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
