1b206d8f187277850b28c576a3895f0b324e6940
Alexandr Sobko <alexfreetibet@gmail.com>
2021-03-26 22:58:50 +0700

    Исправил замечания

--- webapp/model.py
+++ webapp/model.py
@@ -9,63 +9,60 @@ class ModelType(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, index=True, nullable=False)
     brand_id = db.Column(db.Integer, db.ForeignKey('brands.id'), nullable=False)
-    addresses = db.relationship('adverts', backref='modeltypes', lazy=True)
+    brand = db.relationship('brand', backref='model_types')
+
+
+class BaseParam:
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, index=True, nullable=False)
 
 
 class Brand(db.Model):
     __tablename__ = 'brands'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, index=True, nullable=False)
-    addresses = db.relationship('modeltypes', backref='brands', lazy=True)
 
 
 class FuelType(db.Model):
     __tablename__ = 'fueltypes'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, index=True, nullable=False)
-    addresses = db.relationship('adverts', backref='fueltypes', lazy=True)
 
 
 class Transmission(db.Model):
     __tablename__ = 'transmissions'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, index=True, nullable=False)
-    addresses = db.relationship('adverts', backref='transmissions', lazy=True)
 
 
 class WheelsDrive(db.Model):
     __tablename__ = 'wheelsdrives'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, index=True, nullable=False)
-    addresses = db.relationship('adverts', backref='wheelsdrives', lazy=True)
 
 
 class Condition(db.Model):
     __tablename__ = 'conditions'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, index=True, nullable=False)
-    addresses = db.relationship('adverts', backref='conditions', lazy=True)
 
 
 class Body(db.Model):
     __tablename__ = 'bodies'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, index=True, nullable=False)
-    addresses = db.relationship('adverts', backref='bodies', lazy=True)
 
 
 class Color(db.Model):
     __tablename__ = 'colors'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, index=True, nullable=False)
-    addresses = db.relationship('adverts', backref='colors', lazy=True)
 
 
 class City(db.Model):
     __tablename__ = 'cities'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, index=True, nullable=False)
-    addresses = db.relationship('adverts', backref='cities', lazy=True)
 
 
 class Advert(db.Model):
@@ -75,20 +72,36 @@ class Advert(db.Model):
     price = db.Column(db.Integer, index=True, nullable=False)
     date = db.Column(db.DateTime, index=True, nullable=False)
     image_url = db.Column(db.String(255))
-    model_id = db.Column(db.Integer, db.ForeignKey('modeltypes.id'), nullable=False)
     engine_volume = db.Column(db.Float, index=True, nullable=False)
     horse_power = db.Column(db.Integer, index=True, nullable=False)
-    fuel_type_id = db.Column(db.Integer, db.ForeignKey('fueltypes.id'), nullable=False)
-    transmission_id = db.Column(db.Integer, db.ForeignKey('transmissions.id'), nullable=False)
-    wheels_drive_id = db.Column(db.Integer, db.ForeignKey('wheelsdrives.id'), nullable=False)
-    condition_id = db.Column(db.Integer, db.ForeignKey('conditions.id'), nullable=False)
     owners = db.Column(db.String(2), nullable=False)
     year = db.Column(db.Integer, index=True, nullable=False)
     mileage = db.Column(db.Integer, index=True, nullable=False)
-    body_id = db.Column(db.Integer, db.ForeignKey('bodies.id'), nullable=False)
-    color_id = db.Column(db.Integer, db.ForeignKey('colors.id'), nullable=False)
-    city_id = db.Column(db.Integer, db.ForeignKey('cities.id'), nullable=False)
     is_left_hand_drive = db.Column(db.Boolean, nullable=False)
+
+    model_id = db.Column(db.Integer, db.ForeignKey('modeltypes.id'), nullable=False)
+    model = db.relationship('modeltype', backref='advert')
+
+    fuel_type_id = db.Column(db.Integer, db.ForeignKey('fueltypes.id'), nullable=False)
+    fuel_type = db.relationship('fueltype', backref='advert')
+
+    transmission_id = db.Column(db.Integer, db.ForeignKey('transmissions.id'), nullable=False)
+    transmission = db.relationship('transmission', backref='advert')
+
+    wheels_drive_id = db.Column(db.Integer, db.ForeignKey('wheelsdrives.id'), nullable=False)
+    wheels_drive = db.relationship('wheelsdrive', backref='advert')
+
+    condition_id = db.Column(db.Integer, db.ForeignKey('conditions.id'), nullable=False)
+    condition = db.relationship('condition', backref='advert')
+
+    body_id = db.Column(db.Integer, db.ForeignKey('bodies.id'), nullable=False)
+    body = db.relationship('body', backref='advert')
+
+    color_id = db.Column(db.Integer, db.ForeignKey('colors.id'), nullable=False)
+    color = db.relationship('color', backref='advert')
+
+    city_id = db.Column(db.Integer, db.ForeignKey('cities.id'), nullable=False)
+    city = db.relationship('city', backref='advert')
 
     def __repr__(self):
         return f"<Advert:{self.id}: {self.model_id} {self.year}y, price={self.price}>"
