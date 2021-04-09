21f1e711517e88e65b3ce78e25710f1d049147e6
Eduard Shustov <sedfrmmsk@gmail.com>
2021-04-09 12:42:20 +0300

    Все данные для обьявления теперь передаются через render template

--- __init__.py
+++ __init__.py
@@ -7,7 +7,13 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     volumes = [round(x * 0.2, 1) for x in range(1, 40)]
-    return render_template('index.html', len=len(volumes), volumes=volumes)
+    return render_template('index.html', len=len(volumes), volumes=volumes, brand='Toyota',
+        model='Celica', price='630000', city='Москва', year='1995', mileage='221337',
+        volume='3.0', hp='228', fuel_type='Бензин', transmission='Механическая',
+        WD='Задний', wheel_drive='Правый', body='Купе', color='Черный',
+        condition='Не битая', owners='3+',
+        pic_url='https://sun9-39.userapi.com/impg/6NGwMBoUnUhcniGvoBiEJ-yX8SRcQUj020XO-A/NvL8YGVCk0I.jpg?size=2048x1536&quality=96&sign=2028ee00be27a2cd9eac1126b34548ab&type=album',
+        listing_url='https://auto.ru/cars/used/sale/toyota/celica/1103113371-a2d4da4c/')
 
 
 @app.route('/authentication')
