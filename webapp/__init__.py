d642065d9fa328195b11da076b58d1a024776dd8
Alexandr Sobko <alexfreetibet@gmail.com>
2021-04-09 21:46:59 +0700

    на главную страницу теперь отправляются данные из бд. пока все, без разделения на страницы

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from webapp.models import (
     db, Advert, Brand, ModelType, FuelType, Transmission, WheelsDrive, Condition, Body, Color, City
 )
@@ -11,9 +11,42 @@ def create_app():
     db.init_app(app)
 
     @app.route('/')
+    @app.route('/index')
     def index():
-        title = 'CarFound'
-        return title
+        # volumes = [round(x * 0.2, 1) for x in range(1, 40)]
+        adverts = db.session.query(Advert).all()
+        print(adverts)
+        volumes = []
+
+        for advert in adverts:
+            advert_data = {
+                'brand': advert.model.brand.name,
+                'model': advert.model.name,
+                'price': advert.price,
+                'city': advert.city.name,
+                'year': advert.year,
+                'mileage': advert.mileage,
+                'volume': advert.engine_volume,
+                'hp': advert.horse_power,
+                'fuel_type': advert.fuel_type.name,
+                'transmission': advert.transmission.name,
+                'WD': advert.wheels_drive.name,
+                'wheel_drive': 'Левый' if advert.is_left_hand_drive else 'Правый',
+                'body': advert.body.name,
+                'color': advert.color.name,
+                'condition': advert.condition.name,
+                'owners': advert.owners,
+                'pic_url': advert.image_url,
+                'listing_url': advert.url
+            }
+            volumes.append(advert_data)
+        print(volumes)
+
+        return render_template('index.html', len=len(volumes), volumes=volumes)
+
+    @app.route('/authentication')
+    def authentication():
+        return render_template('login.html')
 
     return app
 
