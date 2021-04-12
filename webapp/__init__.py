bd508c27940c154a24217e343ce420fd87dc5db8
Alexandr Sobko <alexfreetibet@gmail.com>
2021-04-12 18:43:50 +0700

    Добавил пагинацию, теперь выводятся по 10 объявлений на странице

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from webapp.models import (
     db, Advert, Brand, ModelType, FuelType, Transmission, WheelsDrive, Condition, Body, Color, City
 )
@@ -13,36 +13,16 @@ def create_app():
     @app.route('/')
     @app.route('/index')
     def index():
-        # volumes = [round(x * 0.2, 1) for x in range(1, 40)]
-        adverts = db.session.query(Advert).all()
-        print(adverts)
-        volumes = []
+        page = request.args.get('page')
+        if page and page.isdigit():
+            page = int(page)
+        else:
+            page = 1
 
-        for advert in adverts:
-            advert_data = {
-                'brand': advert.model.brand.name,
-                'model': advert.model.name,
-                'price': advert.price,
-                'city': advert.city.name,
-                'year': advert.year,
-                'mileage': advert.mileage,
-                'volume': advert.engine_volume,
-                'hp': advert.horse_power,
-                'fuel_type': advert.fuel_type.name,
-                'transmission': advert.transmission.name,
-                'WD': advert.wheels_drive.name,
-                'wheel_drive': 'Левый' if advert.is_left_hand_drive else 'Правый',
-                'body': advert.body.name,
-                'color': advert.color.name,
-                'condition': advert.condition.name,
-                'owners': advert.owners,
-                'pic_url': advert.image_url,
-                'listing_url': advert.url
-            }
-            volumes.append(advert_data)
-        print(volumes)
+        adverts = Advert.query.order_by(Advert.date.desc())
+        pages = adverts.paginate(page=page, per_page=10)
 
-        return render_template('index.html', len=len(volumes), volumes=volumes)
+        return render_template('index.html', pages=pages)
 
     @app.route('/authentication')
     def authentication():
